566450f2895f4701d83b40fb284c4eaaa60a42cd
github username <github email address>
2019-07-26 16:53:53 -0400

    Add show

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -1,8 +1,19 @@
 class AgreementsController < ApplicationController
+  before_action :set_agreement, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @agreements = Agreement.all
   end
 
-  
+  def show
+
+  end
+
+
+  private 
+
+  def set_agreement
+    @agreement = Agreement.find(params[:id])
+  end
 end
