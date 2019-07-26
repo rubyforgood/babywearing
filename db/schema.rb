3860e73dcc4020eddd2e29fbe71e16f0ba4ce47a
Joshua Alley <jauschalley@gmail.com>
2019-07-26 16:08:33 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_07_26_143703) do
     t.datetime "updated_at", null: false
     t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
-  
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
