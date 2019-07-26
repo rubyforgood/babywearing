ee5cbdc8ddea91ba1ea24fb7b8e32935e40f410e
github username <github email address>
2019-07-26 11:54:02 -0400

    Create and update location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,41 @@
 class LocationsController < ApplicationController
 
+  before_action :set_location, only: [:show, :edit, :update]
+
   def index
     @locations = Location.all
   end
 
   def show
+   
+  end
+
+  def new
+    @location = Location.new
+  end
+  
+  def create
+    @location = Location.new(location_params)
+    @location.save
+    redirect_to location_path(@location)
+  end
+
+  def edit 
+
+  end
+
+  def update
+    @location.update(location_params) 
+    redirect_to location_path(@location)
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name)
+  end
+
+  def set_location
     @location = Location.find(params[:id])
   end
 
