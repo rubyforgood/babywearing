077aec4306708bd08e72444f4691175c870cec2f
john-raber <jrab224@gmail.com>
2019-07-26 13:41:06 -0400

    schema formatting fix

--- db/schema.rb
+++ db/schema.rb
@@ -39,5 +39,4 @@ ActiveRecord::Schema.define(version: 2019_07_26_142403) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
