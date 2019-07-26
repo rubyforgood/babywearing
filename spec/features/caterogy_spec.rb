b3690dc245da9c227b8932d71585eedc010f96f7
smurfik <tammy@mlpinit.com>
2019-07-26 13:33:10 -0400

    add specs

---
+++ spec/features/caterogy_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "category", type: :feature do
+  fixtures :categories
+  let!(:category) { categories(:category) }
+
+  scenario "should allow user to create a category" do
+    visit "/categories"
+    find_link("New Category").click
+    fill_in "Name", with: "pineapple"
+    fill_in "Description", with: "sweet"
+    fill_in "Parent", with: "1"
+    click_button "Create Category"
+    expect(page).to have_content "Category was successfully created."
+  end
+
+  scenario "should allow user to update a category" do
+    visit "/categories"
+    find_link("Show").click
+    expect(page).to have_content category.name
+    find_link("Edit").click
+    fill_in "Name", with: "orange"
+    click_button "Update Category"
+    expect(page).to have_content "Category was successfully updated."
+    expect(page).to have_content "orange"
+  end
+
+  scenario "should allow user to delete a category" do
+    visit "/categories"
+    expect(page).to have_content category.name
+    find_link("Destroy").click
+    expect(page).to have_content "Category was successfully destroyed."
+  end
+end
