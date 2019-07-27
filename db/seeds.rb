1a250276f21e3e19583fb5ac1e92146144201c90
john-raber <jrab224@gmail.com>
2019-07-27 14:25:51 -0400

    adding carrier seed data from exported inventory file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require 'csv'
+
+include ActionView::Helpers::SanitizeHelper #included for strip_tags method to remove html from carrier descriptions
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -19,7 +23,7 @@ asian_inspired_carriers.subcategories << Category.create([
 
 buckle_carriers = Category.create(name: 'Buckle Carriers')
 buckle_carriers.subcategories << Category.create([
-  { name: 'Full Buckle Carriers' }, { name: 'Half Bucket Carriers' }, { name: 'Other Buckle Carriers' },
+  { name: 'Full Buckle Carriers' }, { name: 'Half Buckle Carriers' }, { name: 'Other Buckle Carriers' },
   { name: 'Ring Waist Carriers' }, { name: 'Reverse Half Buckle Carriers' }
 ])
 
@@ -44,3 +48,27 @@ Category.find_by_name('Woven Wrap Carriers').subcategories << Category.create([
   { name: 'Chitenges' }, { name: 'Khangas' }, { name: 'Lesos' },
   { name: 'Mantas' }, { name: 'Rebozos' }
 ])
+
+# Importing carriers
+path = File.join Rails.root, 'public', 'inventory.csv'
+csv_text = File.read(path)
+
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  csv_carrier = row.to_hash
+  carrier_params = {
+    item_id: csv_carrier['Item ID'],
+    name: csv_carrier['Name'],
+    manufacturer: csv_carrier['Manufacturer'],
+    model: csv_carrier['Model'],
+    color: csv_carrier['Color'],
+    size: csv_carrier['Size'],
+    location_id: Location.find_or_create_by(name: csv_carrier['Home Location']).id,
+    default_loan_length_days: csv_carrier['Default Loan Length'].to_i,
+    category: Category.find_by(name: csv_carrier['Item Type']),
+    # image_url: csv_carrier['Image'],
+    # description: strip_tags(csv_carrier['Description'])
+  }
+
+  Carrier.create!(carrier_params)
+end
