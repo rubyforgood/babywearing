46c5dc941f4ffd778660847840bf25346c43a323
Chris Hoffman <choffman@optoro.com>
2019-07-27 15:00:24 -0400

    generated db/schema.rb (it had a blank line added randomly)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_27_180939) do
 
   # These are extensions that must be enabled in order to support this database
