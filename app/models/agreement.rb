585d4cdb22011ac7ed2656cc1ce4d1fac5398ec5
john-raber <jrab224@gmail.com>
2019-07-28 11:47:11 -0400

    adding checkbox to show whether agreements are signed or not on agreements index page

--- app/models/agreement.rb
+++ app/models/agreement.rb
@@ -1,3 +1,7 @@
 class Agreement < ApplicationRecord
   validates_presence_of :title, :content
+
+  def signed?(user_id)
+    SignedAgreement.where(agreement_id: id, user_id: user_id).present?
+  end
 end
