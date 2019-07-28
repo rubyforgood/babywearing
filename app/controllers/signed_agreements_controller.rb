16178c6846b0f8d30fdcb6d111f157c214f3abae
john-raber <jrab224@gmail.com>
2019-07-28 11:19:09 -0400

    adding show and create actions in controller

---
+++ app/controllers/signed_agreements_controller.rb
@@ -0,0 +1,33 @@
+class SignedAgreementsController < ApplicationController
+  before_action :set_agreement, only: [:new, :show]
+
+  def show
+    @signed_agreement = SignedAgreement.new
+  end
+
+  def create
+    @signed_agreement = SignedAgreement.new(signed_agreement_params)
+    
+    respond_to do |format|
+      if @signed_agreement.save
+        format.html { redirect_to agreements_path, notice: 'Agreement was successfully signed.' }
+      else
+        format.html { redirect_to signed_agreement_path(@signed_agreement.agreement_id), notice: @signed_agreement.errors.full_messages }
+      end
+    end
+  end
+
+  private
+
+  def set_agreement
+    @agreement = Agreement.find(params[:id])
+  end
+
+  def signed_agreement_params
+    params.require(:signed_agreement).permit(
+      :signature,
+      :user_id,
+      :agreement_id
+    )
+  end
+end
