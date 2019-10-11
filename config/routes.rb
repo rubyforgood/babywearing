9bd3a07eb065c37d469f843a03abb92e6beeafd5
Elizabeth Prescott <liz@meansdatabase.com, eprescott0216@gmail.com>
2019-10-10 20:46:13 -0400

    Issue #63. Landing page displays inventory

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'carriers#index'
 end
