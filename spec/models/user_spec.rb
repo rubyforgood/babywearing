b64da6bfe4769882c4355c17471f44d17ccb18ea
Mauricio P <mauriciopdvg@gmail.com>
2019-10-19 17:16:00 -0300

    User as a member when signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,20 @@ RSpec.describe User do
       test_user = User.reflect_on_association(:roles)
       expect(test_user.macro).to eq(:has_and_belongs_to_many)
     end
+
+    it 'should be a member' do
+      user = User.create!(
+        email: "alicia@test.com",
+        password: "123abc",
+        full_name: "Alicia Barrett",
+        street_address: "123 street",
+        city: "Atlanta",
+        state: "GA",
+        postal_code: "30030",
+        phone_number: "909-851-9806"
+      )
+
+      expect(user.has_role?(:member)).to be_truthy
+    end
   end
 end
