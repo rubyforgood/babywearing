86e274f0329f6942dfe072b80bad59951fad6e1d
Mauricio P <mauriciopdvg@gmail.com>
2019-11-04 16:12:18 -0300

    Remove responding to json from controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,13 +4,11 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.where(parent_id: nil).includes(:subcategories).order(:name)
   end
 
   # GET /categories/1
-  # GET /categories/1.json
   def show
   end
 
@@ -24,46 +22,33 @@ class CategoriesController < ApplicationController
   end
 
   # POST /categories
-  # POST /categories.json
   def create
     @category = authorize Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
   def update
     authorize @category
 
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.update(category_params)
+      redirect_to @category, notice: 'Category was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
     authorize @category
 
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, alert: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
+    if @category.destroy
+      redirect_to categories_url, alert: 'Category was successfully destroyed.'
     end
   end
 
--- app/controllers/membership_types_controller.rb
+++ app/controllers/membership_types_controller.rb
@@ -4,13 +4,11 @@ class MembershipTypesController < ApplicationController
   before_action :set_membership_type, only: [:show, :edit, :update, :destroy]
 
   # GET /membership_types
-  # GET /membership_types.json
   def index
     @membership_types = MembershipType.all
   end
 
   # GET /membership_types/1
-  # GET /membership_types/1.json
   def show
   end
 
@@ -24,47 +22,33 @@ class MembershipTypesController < ApplicationController
   end
 
   # POST /membership_types
-  # POST /membership_types.json
   def create
     @membership_type = authorize MembershipType.new(membership_type_params)
 
-    respond_to do |format|
-      if @membership_type.save
-        format.html { redirect_to @membership_type, notice: 'Membership type was successfully created.' }
-        format.json { render :show, status: :created, location: @membership_type }
-      else
-        format.html { render :new }
-        format.json { render json: @membership_type.errors, status: :unprocessable_entity }
-      end
+    if @membership_type.save
+      redirect_to @membership_type, notice: 'Membership type was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /membership_types/1
-  # PATCH/PUT /membership_types/1.json
   def update
     authorize @membership_type
 
-    respond_to do |format|
-      if @membership_type.update(membership_type_params)
-        format.html { redirect_to @membership_type, notice: 'Membership type was successfully updated.' }
-        format.json { render :show, status: :ok, location: @membership_type }
-      else
-        format.html { render :edit }
-        format.json { render json: @membership_type.errors, status: :unprocessable_entity }
-      end
+    if @membership_type.update(membership_type_params)
+      redirect_to @membership_type, notice: 'Membership type was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /membership_types/1
-  # DELETE /membership_types/1.json
   def destroy
     authorize @membership_type
 
     if @membership_type.destroy
-      respond_to do |format|
-        format.html { redirect_to membership_types_url, alert: 'Membership type was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+      redirect_to membership_types_url, alert: 'Membership type was successfully destroyed.'
     end
   end
 
