c2322cdbbbbde5831f77d075acb2e858e71e0729
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-11-21 11:47:07 -0300

    Fix rubocop of spec_loan_policy.rb (#227)

--- spec/policies/loan_policy_spec.rb
+++ spec/policies/loan_policy_spec.rb
@@ -8,16 +8,10 @@ RSpec.describe LoanPolicy do
   let(:member) { users(:member) }
 
   permissions :create? do
-    it "denies access if user is not an admin or volunteer" do
-      expect(subject).not_to permit(member)
-    end
+    it { is_expected.not_to permit(member) }
 
-    it "grants access if the user is an admin" do
-      expect(subject).to permit(admin)
-    end
+    it { is_expected.to permit(admin) }
 
-    it "grants access if the user is a volunteer" do
-      expect(subject).to permit(volunteer)
-    end
+    it { is_expected.to permit(volunteer) }
   end
 end
