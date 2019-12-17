8a3fc8d3fbc19af6689881e8b516b7f12eddcce2
mark100net <mark@pokervane.com>
2019-12-16 20:48:11 -0800

    restyle signup form

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -4,16 +4,16 @@ RSpec.feature "user registration" do
   scenario "should allow user to create a user account" do
     visit "/"
     find_link("Sign up", match: :first).click
-    fill_in "Email", with: "alicia@gmail.com"
+    fill_in "Enter email address", with: "alicia@gmail.com"
     fill_in "Password", with: "just4now"
-    fill_in "Password confirmation", with: "just4now"
+    fill_in "Confirm Password", with: "just4now"
     fill_in "First name", with: "me"
     fill_in "Last name", with: "last"
     fill_in "Street address", with: "123 main street"
     fill_in "City", with: "fairfax"
     fill_in "State", with: "VA"
     fill_in "Postal code", with: "22032"
-    fill_in "Phone number", with: "8008885555"
+    fill_in "Phone", with: "8008885555"
     click_button "Sign up"
     expect(page).to have_content("Welcome! You have signed up successfully.", count: 1)
   end
@@ -22,9 +22,9 @@ RSpec.feature "user registration" do
     scenario "should NOT allow user to create a user account" do
       visit "/"
       find_link("Sign up", match: :first).click
-      fill_in "Email", with: "alicia.email.com"
+      fill_in "Enter email address", with: "alicia.email.com"
       fill_in "Password", with: "just4now"
-      fill_in "Password confirmation", with: "just4now"
+      fill_in "Confirm Password", with: "just4now"
       click_button "Sign up"
       expect(page).to have_content "Email is invalid"
     end
